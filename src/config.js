9fbd6c6c8193a33e4acdabb4eb1301b42a3bbce5
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-02-12 01:18:50 +0530

    Revert "url added"
    This reverts commit b88628fe05a09fa17b8717a0b3c75fcfcb8a1bcd.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,2 @@
-// export const API_BASE_URL = 'http://localhost:5000/api'; 
-// export const API_BASE_URL_IMAGE = 'http://localhost:5000';
-
-
-export const API_BASE_URL = 'https://inventory-backend-lovat.vercel.app/api'; 
-export const API_BASE_URL_IMAGE = 'https://inventory-backend-lovat.vercel.app';
+export const API_BASE_URL = 'http://localhost:5000/api'; 
+export const API_BASE_URL_IMAGE = 'http://localhost:5000';
