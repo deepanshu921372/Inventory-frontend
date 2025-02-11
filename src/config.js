b88628fe05a09fa17b8717a0b3c75fcfcb8a1bcd
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-02-12 00:57:51 +0530

    url added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
-export const API_BASE_URL = 'http://localhost:5000/api'; 
-export const API_BASE_URL_IMAGE = 'http://localhost:5000';
+// export const API_BASE_URL = 'http://localhost:5000/api'; 
+// export const API_BASE_URL_IMAGE = 'http://localhost:5000';
+
+
+export const API_BASE_URL = 'https://inventory-backend-lovat.vercel.app/api'; 
+export const API_BASE_URL_IMAGE = 'https://inventory-backend-lovat.vercel.app';
